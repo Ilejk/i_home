60c4b0176fde504496a96965a846f534601736e1
Ilejk <adrianskibaa@gmail.com>
2023-06-08 18:18:33 +0200

    08.06.2023-18:18

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:i_home/presentation/core/app_widget.dart';
+import 'package:i_home/firebase_options.dart';
+import 'package:i_home/src/injection.dart';
+import 'package:i_home/src/presentation/core/app_widget.dart';
 import 'package:injectable/injectable.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // configureInjection(Environment.prod);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  configureInjection(Environment.dev);
   runApp(
     const AppWidget(),
   );
